6d010c061181c45a5509f698a76b020f316f532b
eepycat <erotic.eepycat@gmail.com>
2026-01-13 09:58:41 +0200

    i dont even remember from yersterday

--- Program.cs
+++ Program.cs
@@ -4,21 +4,25 @@ using webdotnetapp.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Modern OpenAPI
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
-
+// Development tools
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.MapOpenApi(); // <-- modern replacement for UseSwagger + UseSwaggerUI
 }
 
-app.UseStaticFiles(); 
+// Serve frontend
+app.UseDefaultFiles(); // serves index.html
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
